daa0d6f9b26ca06439df9fa4bff20b5b73c3c788
alice reuter <alice@alicereuter.com>
2020-01-16 16:45:48 -0800

    changed offsets

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,11 +1,14 @@
 description[0]="Smasher of stacks"
 description[1]="I <3 systems"
 
+offset[0]="80"
+offset[1]="100"
 
 size=${#description[@]}
 index=$(($RANDOM % $size))
 
 sed -i "s|TAGLINE|${description[$index]}|" "./config.toml"
+sed -i "s|OFFSET|${offset[$index]}|" "./configtoml"
 cd content || exit
 find . -name '*.md' | while read -r file; do
 	date="$(git log -1 --format=%ci "$file")"
