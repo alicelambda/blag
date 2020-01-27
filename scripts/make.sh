a9bf5b722d8dada25a8320d0766d40f47a999286
Alice Reuter <alice@alicereuter.com>
2020-01-26 23:06:35 -0600

    fwixed things

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,11 +1,14 @@
 description[0]="Smasher of stacks"
 description[1]="I <3 systems"
 
+offset[0]="220"
+offset[1]="125"
 
 size=${#description[@]}
 index=$(($RANDOM % $size))
 
 sed -i "s|TAGLINE|${description[$index]}|" "./config.toml"
+sed -i "s|OFFSET|${offset[$index]}|" "./themes/solar-theme-hugo/layouts/_default/list.html"
 cd content || exit
 find . -name '*.md' | while read -r file; do
 	date="$(git log -1 --format=%ci "$file")"
